7d9a751b47e5990575a3870cab45f8cf02178649
Lee Maguire <leemaguire@Lees-MacBook-Pro.local>
2017-01-06 14:24:38 +0000

    update build script

--- CartrawlerSDK/CI/Cocoapod.sh
+++ CartrawlerSDK/CI/Cocoapod.sh
@@ -36,5 +36,4 @@ sed -i .temp "s/FRAMEWORK_NAME/${FRAMEWORK_NAME}/g; s/FRAMEWORK_VERSION/${BUILD_
 cd "${PROJECT_DIR}/"
 echo "Pushing pod spec ${PROJECT_DIR}/${FRAMEWORK_NAME}.podspec"
 export LANG=en_US.UTF-8
-echo "pod repo push cartrawlerpods ${PROJECT_DIR}/${FRAMEWORK_NAME}.podspec"
-pod repo push cartrawlerpods ${PROJECT_DIR}/${FRAMEWORK_NAME}.podspec
+/usr/bin/pod repo push cartrawlerpods ${PROJECT_DIR}/${FRAMEWORK_NAME}.podspec
