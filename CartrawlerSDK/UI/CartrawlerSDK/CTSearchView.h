6c77b524711ade45ba60313009154e7f226d6924
Lee Maguire <lmaguire@cartrawler.com>
2017-03-29 11:58:57 +0100

    RYP4-13 initial thoughts

---
+++ CartrawlerSDK/UI/CartrawlerSDK/CTSearchView.h
@@ -0,0 +1,13 @@
+//
+//  CTSearchView.h
+//  CartrawlerRental
+//
+//  Created by Lee Maguire on 29/03/2017.
+//  Copyright © 2017 Cartrawler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTSearchView : UIView
+
+@end
---
+++ CartrawlerSDK/UI/CartrawlerSDK/CTSearchView.m
@@ -0,0 +1,26 @@
+//
+//  CTSearchView.m
+//  CartrawlerRental
+//
+//  Created by Lee Maguire on 29/03/2017.
+//  Copyright © 2017 Cartrawler. All rights reserved.
+//
+
+#import "CTSearchView.h"
+
+@implementation CTSearchView
+
+- (instancetype)init
+{
+    self = [super init];
+    
+    return self;
+}
+
+- (void)updateDisplayWithSearch:(NSObject *)search
+{
+    
+}
+
+
+@end
