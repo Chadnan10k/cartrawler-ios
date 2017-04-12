acdd7b93a2a861baa65a96e67e71026eff69867c
Alan Mathews <amathews@cartrawler.com>
2017-04-12 08:32:54 -0700

    RYP4-98 Added extras horizontal view container

---
+++ CartrawlerSDK/UI/CartrawlerSDK/CTExtrasCarouselView.h
@@ -0,0 +1,48 @@
+//
+//  CTExtrasCarouselView.h
+//  CartrawlerRental
+//
+//  Created by Alan on 11/04/2017.
+//  Copyright © 2017 Cartrawler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "CTExtrasCollectionView.h"
+
+@class CTExtrasCarouselView;
+
+/**
+ Delegate which is notified when View All is selected
+ */
+@protocol CTExtrasCarouselViewDelegate
+
+/**
+ Implement this to be notified when View All is selected
+
+ @param extrasView the extras view
+ */
+- (void)extrasViewDidTapViewAll:(CTExtrasCarouselView *)extrasView;
+
+@end
+
+/**
+ Container view for a CTExtrasCollectionView with horizontal scrolling
+ 
+ Includes button to view all extras
+ */
+@interface CTExtrasCarouselView : UIView
+
+/**
+ Delegate to be notified of view all selection
+ */
+@property (nonatomic, weak) id <CTExtrasCarouselViewDelegate> delegate;
+
+/**
+ Designated Initialiser
+
+ @param extras an array of CTExtraEquipment objects
+ @return a CTExtrasCarouselView instance
+ */
+- (instancetype)initWithExtras:(NSArray *)extras;
+
+@end
---
+++ CartrawlerSDK/UI/CartrawlerSDK/CTExtrasCarouselView.m
@@ -0,0 +1,72 @@
+//
+//  CTExtrasCarouselView.m
+//  CartrawlerRental
+//
+//  Created by Alan on 11/04/2017.
+//  Copyright © 2017 Cartrawler. All rights reserved.
+//
+
+#import "CTExtrasCarouselView.h"
+#import <CartrawlerSDK/CTLabel.h>
+#import <CartrawlerSDK/CTAlertViewController.h>
+
+@interface CTExtrasCarouselView ()
+@property (nonatomic, strong) CTLabel *titleLabel;
+@property (nonatomic, strong) UIButton *viewAllButton;
+@property (nonatomic, strong) CTExtrasCollectionView *collectionView;
+@end
+
+@implementation CTExtrasCarouselView
+
+- (instancetype)initWithExtras:(NSArray *)extras {
+    self = [super init];
+    if (self) {
+        self.titleLabel = [[CTLabel alloc] init:16 textColor:[UIColor blackColor] textAlignment:NSTextAlignmentCenter boldFont:YES];
+        self.titleLabel.translatesAutoresizingMaskIntoConstraints = NO;
+        self.titleLabel.text = @"Add Extras";
+        self.titleLabel.textAlignment = NSTextAlignmentLeft;
+        [self addSubview:self.titleLabel];
+        
+        self.collectionView = [[CTExtrasCollectionView alloc] initWithScrollDirection:UICollectionViewScrollDirectionHorizontal];
+        [self.collectionView updateWithExtras:extras];
+        self.collectionView.translatesAutoresizingMaskIntoConstraints = NO;
+        [self addSubview:self.collectionView];
+        
+        self.viewAllButton = [UIButton new];
+        self.viewAllButton.translatesAutoresizingMaskIntoConstraints = NO;
+        [self.viewAllButton setTitle:@"View All" forState:UIControlStateNormal];
+        self.viewAllButton.titleLabel.textColor = [UIColor colorWithRed:43.0/255.0 green:147.0/255.0 blue:232.0/255.0 alpha:1.0];
+        [self addSubview:self.viewAllButton];
+        self.viewAllButton.backgroundColor = [UIColor redColor];
+        [self.viewAllButton addTarget:self action:@selector(didTapViewAll:) forControlEvents:UIControlEventTouchUpInside];
+        
+        [self addConstraints];
+    }
+    return self;
+}
+
+- (void)addConstraints {
+    NSDictionary *views = @{@"titleLabel": self.titleLabel, @"collectionView": self.collectionView, @"viewAllButton": self.viewAllButton};
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[titleLabel]-[viewAllButton(80)]-|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|[collectionView]|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(10)-[titleLabel(34)]-(10)-[collectionView(120)]|"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:views]];
+    [self addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-(10)-[viewAllButton(34)]"
+                                                                 options:0
+                                                                 metrics:nil
+                                                                   views:views]];
+}
+
+- (void)didTapViewAll:(UIButton *)button {
+    [self.delegate extrasViewDidTapViewAll:self];
+}
+
+@end
