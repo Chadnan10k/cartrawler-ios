3c01926e683726f173e189a787eaac13ede037d7
Lee Maguire <lmaguire@cartrawler.com>
2017-04-20 17:46:53 +0100

    RYP4-11 Converting to MVP architecture for vehicle selection / detail

---
+++ CartrawlerSDK/UI/CartrawlerSDK/CTVehiclePresenterViewController.h
@@ -0,0 +1,13 @@
+//
+//  CTVehiclePresenterViewController.h
+//  CartrawlerRental
+//
+//  Created by Lee Maguire on 20/04/2017.
+//  Copyright © 2017 Cartrawler. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CTVehiclePresenterViewController : UIViewController
+
+@end
---
+++ CartrawlerSDK/UI/CartrawlerSDK/CTVehiclePresenterViewController.m
@@ -0,0 +1,37 @@
+//
+//  CTVehiclePresenterViewController.m
+//  CartrawlerRental
+//
+//  Created by Lee Maguire on 20/04/2017.
+//  Copyright © 2017 Cartrawler. All rights reserved.
+//
+
+#import "CTVehiclePresenterViewController.h"
+
+@interface CTVehiclePresenterViewController ()
+
+@end
+
+@implementation CTVehiclePresenterViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
